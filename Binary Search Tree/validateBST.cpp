e6245e0bfcb98258f11516836549cd6f614fea10
Kaustav Mukhopadhyay <mu.kaustav@gmail.com>
2021-10-21 03:00:44 +0530

    remove useless code

--- Binary Search Tree/validateBST.cpp
+++ Binary Search Tree/validateBST.cpp
@@ -28,68 +28,6 @@ Node *insertBST(Node *root, int val)
     return root;
 }
 
-Node *inorderSuccessor(Node *root)
-{
-    Node *curr = root->right;
-
-    while (curr->left)
-        curr = curr->left;
-
-    return curr;
-}
-
-Node *deleteBST(Node *root, int val)
-{
-    if (!root)
-        return NULL;
-
-    if (val < root->data)
-        root->left = deleteBST(root->left, val);
-
-    else if (val > root->data)
-        root->right = deleteBST(root->right, val);
-
-    else
-    {
-        if (!root->left)
-        {
-            Node *temp = temp->right;
-            delete root;
-            return temp;
-        }
-
-        else if (!root->right)
-        {
-            Node *temp = temp->left;
-            delete root;
-            return temp;
-        }
-
-        else
-        {
-            Node *temp = inorderSuccessor(root);
-            swap(root->data, temp->data);
-            root->right = deleteBST(root->right, val);
-        }
-    }
-
-    return root;
-}
-
-int searchBST(Node *root, int val)
-{
-    if (!root)
-        return -1;
-
-    if (root->data == val)
-        return root->data;
-
-    if (val < root->data)
-        return searchBST(root->left, val);
-
-    return searchBST(root->right, val);
-}
-
 void inorder(Node *root)
 {
     if (!root)
